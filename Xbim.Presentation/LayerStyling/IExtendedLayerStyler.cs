74167ce2daf984b1ce7d269fe77fbf2f2d725fdc
Claudio Benghi <claudio.benghi@gmail.com>
2015-07-03 22:33:25 +0200

    Towards Net40 (presentation should be ok, xplorer to fix).

---
+++ Xbim.Presentation/LayerStyling/IExtendedLayerStyler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Xbim.Presentation.LayerStyling;
+using Xbim.XbimExtensions.Interfaces;
+
+namespace XbimLibrary.Xbim.Presentation.Extensions.LayerStyler
+{
+    public interface IExtendedLayerStyler : ILayerStyler
+    {
+        HashSet<Type> HiddenTypes { get; }
+
+        void SetModelStyle(bool status);
+
+        bool NeedsGeometryComputation { get; }
+
+        void Show(IEnumerable<IPersistIfcEntity> entities);
+
+        void Isolate(IEnumerable<IPersistIfcEntity> entities);
+
+        void Hide(IEnumerable<IPersistIfcEntity> entities);
+
+        void Show(Type ifcType);
+
+        void Isolate(Type ifcType);
+
+        void Hide(Type ifcType);
+
+        void ResetAll();
+
+        bool IsHidden(IPersistIfcEntity entity);
+    }
+}
