118a83b775c4882e30b10161fafc7446c06b10c0
Andy Ward <andy.ward@xbim.net>
2023-01-02 18:34:38 +0000

    Fixed up Tests, Winforms and Plugin projects Tidy up of projects

--- Xbim.Presentation/Properties/AssemblyInfo.cs
+++ Xbim.Presentation/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Windows.Markup;
 [assembly: Guid("603a1283-949b-4733-bb77-0c622e46346d")]
 
 [assembly: XmlnsDefinition("http://schemas.Xbim.com/Presentation", "Xbim.Presentation")]
-[assembly: InternalsVisibleTo("Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010029a3c6da60efcb3ebe48c3ce14a169b5fa08ffbf5f276392ffb2006a9a2d596f5929cf0e68568d14ac7cbe334440ca0b182be7fa6896d2a73036f24bca081b2427a8dec5689a97f3d62547acd5d471ee9f379540f338bbb0ae6a165b44b1ae34405624baa4388404bce6d3e30de128cec379147af363ce9c5845f4f92d405ed0")]
+[assembly: InternalsVisibleTo("Tests")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,
