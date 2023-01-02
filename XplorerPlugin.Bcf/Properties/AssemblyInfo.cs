b72b46fb3cde3eb3fd1d8cd1b6728db228cf865b
Andy Ward <andy.ward@xbim.net>
2023-01-02 18:34:38 +0000

    Fixed up Tests, Winforms and Plugin projects Tidy up of projects

--- XplorerPlugin.Bcf/Properties/AssemblyInfo.cs
+++ XplorerPlugin.Bcf/Properties/AssemblyInfo.cs
@@ -7,7 +7,6 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("xBIM BCF Plugin")]
 [assembly: AssemblyDescription("")]
 
 
@@ -15,16 +14,6 @@ using System.Windows;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -35,4 +24,3 @@ using System.Windows;
     // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyFileVersion("5.0.1811.26661")]
